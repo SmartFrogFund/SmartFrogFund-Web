8a45d833889a053daca3b18884bde99f5af5bdc6
Ethan Zhu <zjh1429823270@gmail.com>
2024-06-24 21:52:53 +0800

    feat: add category

--- src/app/home/_components/frog-projects/frog-projects.tsx
+++ src/app/home/_components/frog-projects/frog-projects.tsx
@@ -1,17 +1,29 @@
 import React, { FC } from "react";
+import { Empty } from "antd";
 import FrogItem from "./frog-item";
+import Loading from "../../lodaing";
 import { IFrogItem } from "../../interface/frog-project";
 
 type IProps = {
-  projectList: Array<IFrogItem>
+  projectList: Array<IFrogItem>,
+  isLoading: boolean
 }
 
-const FrogProjects: FC<IProps> = ({ projectList }) => (
-  <div className="flex flex-col gap-2 max-w-screen-md m-auto mt-2 mb-8">
-    {projectList.map((item) => (
-      <FrogItem key={item.id} frogData={item} />
-    ))}
-  </div>
-);
+const FrogProjects: FC<IProps> = ({
+  projectList, isLoading,
+}) => {
+  if (isLoading) {
+    return (
+      <Loading />
+    );
+  }
+  return (
+    <div className="flex flex-col gap-2 max-w-screen-md m-auto mt-2 mb-8">
+      {projectList.length === 0 ? <Empty style={{ marginTop: 200 }} description={<span className="text-white">No data</span>} /> : projectList.map((item) => (
+        <FrogItem key={item.id} frogData={item} />
+      ))}
+    </div>
+  );
+};
 
 export default FrogProjects;
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 import React from "react";
+import { useAccount } from "wagmi";
 import { useTheGraph } from "@/hooks/useGraph";
 import Header from "./_components/header";
 import ActionBar from "./_components/action-bar";
 import FrogProjects from "./_components/frog-projects/frog-projects";
 import LogoBg from "../../public/images/logo-bg-frog-rocket.png";
-import { IFrogItem, IFundedItem } from "./interface/frog-project";
+import { IFrogItem, IFundedItem, ITabs } from "./interface/frog-project";
 
 const graphApiUrl = process.env.NEXT_PUBLIC_GRAPH_API_URL;
 
 function HomePage() {
+  const { address } = useAccount();
+  const [curTab, setCurTab] = React.useState<ITabs>("ALL");
   const [projectList, setProjectList] = React.useState<Array<IFrogItem>>([]);
   const [fundedList, setFundedList] = React.useState<Array<IFundedItem>>([]);
   const {
@@ -18,7 +21,7 @@ function HomePage() {
     loading,
   }: { data: any; loading: boolean; error: any } = useTheGraph({
     url: graphApiUrl || "",
-    query: ` 
+    query: `
     {
       projectCreateds {
         _description
@@ -34,14 +37,8 @@ function HomePage() {
         transactionHash
       }
       projectFundeds {
-        amount
-        blockNumber
-        blockTimestamp
-        id
-        isEth
-        projectId
         supporter
-        transactionHash
+        projectId
       }
     }
     `,
@@ -51,6 +48,27 @@ function HomePage() {
     setProjectList(projectCreateds || []);
     setFundedList(projectFundeds || []);
   }, [resData]);
+  const handleChangeTab = (tab:ITabs) => setCurTab(tab);
+  const isLogged = Boolean(address);
+  const myProjects: Array<IFrogItem> = []; // 我发起的项目
+  const myFundedProjects: Array<IFrogItem> = []; // 我投资的项目
+  const myFundedProjectIds: Array<string> = [];
+  if (isLogged) {
+    fundedList.forEach((item) => {
+      if (item.supporter === address) {
+        myFundedProjectIds.push(item.projectId);
+      }
+    });
+    projectList.forEach((item) => {
+      if (item.creator === address) myProjects.push(item);
+      if (myFundedProjectIds.includes(item.projectId)) myFundedProjects.push(item);
+    });
+  }
+  const projectsData = {
+    ALL: projectList,
+    MY_PROJECT: myProjects,
+    MY_FUND: myFundedProjects,
+  };
   return (
     <div
       className="bg-[#0F030F] h-full text-white bg-no-repeat bg-fixed overflow-y-scroll"
@@ -61,8 +79,8 @@ function HomePage() {
       }}
     >
       <Header />
-      <ActionBar />
-      <FrogProjects projectList={projectList} />
+      <ActionBar isLogged={isLogged} curTab={curTab} onChangeTab={handleChangeTab} />
+      <FrogProjects projectList={projectsData[curTab]} isLoading={loading} />
     </div>
   );
 }
