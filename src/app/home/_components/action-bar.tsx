40256dfda93b45fe720fb959717e98dcc24d753b
尹雪剑 <gony952701@gmail.com>
2024-06-25 17:36:29 +0800

    style: tabs

--- src/app/home/_components/action-bar.tsx
+++ src/app/home/_components/action-bar.tsx
@@ -7,17 +7,17 @@ type IProps = {
   curTab: ITabs,
   onChangeTab: (tab: ITabs) => void
 }
-const commonClassNames = "inline-block mr-2 rounded-2xl leading-8 px-3 text-center hover:scale-105";
+const commonClassNames = "inline-block mr-2  leading-8 px-3 text-center hover:scale-105";
 const ActionBar:FC<IProps> = ({ isLogged, curTab, onChangeTab }) => (
   <div className="top-65 z-40 w-full bg-[#0F030F] h-[40px]">
     <div className="h-full max-w-screen-md m-auto flex flex-row items-center justify-between">
       <div>
-        <button onClick={() => onChangeTab("ALL")} className={`${commonClassNames} ${curTab === "ALL" ? "bg-[#4caf50]" : ""}`}>All project</button>
+        <button onClick={() => onChangeTab("ALL")} className={`${commonClassNames} ${curTab === "ALL" ? "border-b-2 border-[#4caf50]" : ""}`}>All project</button>
         {
           isLogged && (
             <>
-              <button onClick={() => onChangeTab("MY_PROJECT")} className={`${commonClassNames} ${curTab === "MY_PROJECT" ? "bg-[#4caf50]" : ""}`}>My project</button>
-              <button onClick={() => onChangeTab("MY_FUND")} className={`${commonClassNames} ${curTab === "MY_FUND" ? "bg-[#4caf50]" : ""}`}>My fund</button>
+              <button onClick={() => onChangeTab("MY_PROJECT")} className={`${commonClassNames} ${curTab === "MY_PROJECT" ? "border-b-2 border-[#4caf50]" : ""}`}>My project</button>
+              <button onClick={() => onChangeTab("MY_FUND")} className={`${commonClassNames} ${curTab === "MY_FUND" ? "border-b-2 border-[#4caf50]" : ""}`}>My fund</button>
             </>
           )
         }
